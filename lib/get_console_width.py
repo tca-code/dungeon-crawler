8bbb32a031a71cfd2108ab30bfe67e00ee798799
Matt McElwee <mmcelwee@dmsi.com>
2019-12-11 16:05:39 -0600

    Corrected the windows issue

--- lib/get_console_width.py
+++ lib/get_console_width.py
@@ -1,5 +1,12 @@
 import os
+import re
 
 def get_console_width():
-    coords = os.popen("stty size", "r").read().split()
-    return int(coords[1])
+    if os.name == "nt":
+        regexr = r"Lines: (\s)* (\d*)"
+        output = os.popen("MODE", "r").read()
+        match = re.split(regexr, output)
+        return int(coords[2])
+    else:
+        coords = os.popen("stty size", "r").read().split()
+        return int(coords[1])
